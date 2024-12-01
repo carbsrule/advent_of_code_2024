974732757895ba0a36805ebe38cdeeddbf7106b7
Benno Lang <transmogribenno@gmail.com>
2024-12-01 17:44:13 +1030

    Day 1 part 1 solution

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,8 +1,19 @@
+use crate::lines;
+
 mod day01;
 
 pub fn run(day: u8, part: u8) {
+    let mut lines: Vec<String> = Vec::new();
+    loop {
+        let (bytes, line) = lines::read_line();
+        if bytes < 1 {
+            break;
+        }
+        lines.push(line)
+    }
+
     match (day, part) {
-        (1, 1) => day01::part1(),
+        (1, 1) => day01::part1(lines),
         _ => println!("Implementation not available"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod args;
 mod days;
+mod lines;
 
 fn main() {
     let (day, part) = args::get_day_part();
