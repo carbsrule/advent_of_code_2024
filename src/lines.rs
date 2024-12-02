46b235233880191960be2d7e0b443ee245ab2822
Benno Lang <transmogribenno@gmail.com>
2024-12-02 17:33:53 +1030

    Remove unused code
    This was meant to be removed in 33aec0e4

--- src/lines.rs
+++ src/lines.rs
@@ -1,16 +1,4 @@
-use std::env;
 use std::io;
-
-pub fn get_part() -> u32 {
-    let part: u32 = env::args().nth(1)
-        .unwrap_or("1".to_string())
-        .parse()
-        .expect("Failed to parse part arg");
-    if part != 2 {
-        return 1;
-    }
-    return part;
-}
 
 pub fn read_line() -> (usize, String) {
     let mut line = String::new();
@@ -23,14 +11,4 @@ pub fn read_line() -> (usize, String) {
         line = line.trim().to_string();
     }
     return (num_bytes, line);
-}
-
-pub fn parse_int(num: &str) -> i32 {
-    return num.parse().expect("Failed to parse number");
-}
-
-pub fn char_parse_int(ch: &char) -> i32 {
-    let mut num = String::new();
-    num.push(*ch);
-    return parse_int(&num);
 }
